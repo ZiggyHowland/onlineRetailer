70010120f72edd5489582f58c4e72d634c53e3c0
Andy Olsen <andyo@olsensoft.com>
2021-03-15 22:12:09 +0000

    Update ProductService.java

--- src/main/java/no/dnb/reskill/onlineretailer/ProductService.java
+++ src/main/java/no/dnb/reskill/onlineretailer/ProductService.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 
 // Represent business logic
 public interface ProductService {
+    
+    // FROM ANDY: Generally it's OK to take an entire Product object here rather than separate params. 
     void addToStock(String name, double price);
     void deleteFromStock(int productId);
     void updateStock(Product product);
