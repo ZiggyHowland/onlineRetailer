00bafd01024cd625b17f24c3c86f4c79a8764ef9
ZiggyHowland <sigbjorn@hovland.nu>
2021-03-15 13:07:26 +0100

    Initial commit

---
+++ src/main/java/no/dnb/reskill/onlineretailer/OnlineRetailerApplication.java
@@ -0,0 +1,13 @@
+package no.dnb.reskill.onlineretailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRetailerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineRetailerApplication.class, args);
+    }
+
+}
